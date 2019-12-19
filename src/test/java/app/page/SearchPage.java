67c7f655e93521c40715a5dcfe0e82a6403fd026
q8844240 <546038704@qq.com>
2019-12-19 18:02:03 +0800

    增加case

---
+++ src/test/java/app/page/SearchPage.java
@@ -0,0 +1,24 @@
+package app.page;
+
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+
+public class SearchPage {
+
+    private By inputBox = By.id("com.xueqiu.android:id/search_input_text");
+
+    public SearchPage search(String keyWord){
+
+        App.driver.findElement(inputBox).sendKeys(keyWord);
+        MobileElement el5 = (MobileElement) App.driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.LinearLayout/android.widget.TextView[1]");
+        el5.click();
+        return this;
+    }
+
+    public Integer getCurrentPrice(){
+
+        MobileElement el6 = (MobileElement) App.driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/android.widget.RelativeLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.RelativeLayout");
+        el6.click();
+        return Integer.valueOf(el6.getText());
+    }
+}
--- src/test/java/app/testcase/TestSearch.java
+++ src/test/java/app/testcase/TestSearch.java
@@ -1,4 +1,28 @@
 package app.testcase;
 
+import app.page.App;
+import app.page.SearchPage;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class TestSearch {
+
+    public static SearchPage searchPage;
+
+    @BeforeClass
+    public void beforeAll() throws MalformedURLException {
+        App.start();
+        searchPage=App.toSearch();
+    }
+
+    @Test
+    public void search() throws MalformedURLException {
+        assertThat(searchPage.search("alibaba").getCurrentPrice(),equalTo(100));
+    }
 }
