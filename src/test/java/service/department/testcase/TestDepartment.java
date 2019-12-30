5b166ed70227b417c79c9f060042269cf29dce57
q8844240 <546038704@qq.com>
2019-12-30 22:15:14 +0800

    mac tijiao

--- src/test/java/service/department/testcase/TestDepartment.java
+++ src/test/java/service/department/testcase/TestDepartment.java
@@ -1,15 +1,29 @@
 package service.department.testcase;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import service.department.api.Department;
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 public class TestDepartment {
 
 
     static Department department = new Department();
+
+    @BeforeClass
+    public static void beforeClass(){
+        ArrayList<Integer> ids = department.list(department.parentDepartid).then()
+                .extract().body().path("department.findAll{d->d.parentid==" + department.parentDepartid + "}.id");
+        System.out.println(ids);
+        ids.forEach(id->department.delete(id));
+    }
+
     @Test
     public void list(){
         department.list(department.parentDepartid)
@@ -18,7 +32,9 @@ public class TestDepartment {
 
     @Test
     public void create(){
-        department.deparCreate("种花兔4").then().body("errmsg",equalTo("created"));
+        String name = "种花兔3";
+        department.deparCreate(name).then().body("errmsg",equalTo("created"));
+        department.list(department.parentDepartid).then().body("department.id",hasSize(2));
     }
 
     @Test
