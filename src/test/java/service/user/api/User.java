75c4b6e6dddf2faa9f320182d17642b27ce4b953
q8844240 <546038704@qq.com>
2020-01-05 21:17:38 +0800

    增加模板

--- src/test/java/service/user/api/User.java
+++ src/test/java/service/user/api/User.java
@@ -1,9 +1,15 @@
 package service.user.api;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import service.Work;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
@@ -58,6 +64,45 @@ public class User {
                 .queryParam("access_token", Work.getInstance().getToken())
                 .body(data)
                 .when().log().all().post("https://qyapi.weixin.qq.com/cgi-bin/user/create")
+                .then().log().all().extract().response();
+    }
+
+    public Response clone(String userid, HashMap<String, Object> data) {
+        data.put("userid", userid);
+
+        //todo:使用模板技术
+        String body = template("/service/user/api/user.json",data);
+
+        return given()
+                .queryParam("access_token", Work.getInstance().getToken())
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when().log().all().post("https://qyapi.weixin.qq.com/cgi-bin/user/create")
+                .then().log().all().extract().response();
+    }
+
+    public String template(String templatePath,HashMap<String, Object> data){
+
+        Writer writer = new StringWriter();
+        MustacheFactory mf = new DefaultMustacheFactory();
+        ///Users/mac/IdeaProjects/BCAutomation/BCAutomation/src/main/resources/service/user/api/user.json
+        Mustache mustache = mf.compile(this.getClass().getResource(templatePath).getPath());
+        mustache.execute(writer, data);
+        try {
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return writer.toString();
+
+    }
+
+    public Response delete(String userid) {
+
+        return given()
+                .queryParam("access_token", Work.getInstance().getToken())
+                .queryParam("userid", userid)
+                .when().log().all().post("https://qyapi.weixin.qq.com/cgi-bin/user/delete")
                 .then().log().all().extract().response();
     }
 
--- src/test/java/service/user/testcase/TestUser.java
+++ src/test/java/service/user/testcase/TestUser.java
@@ -1,11 +1,16 @@
 package service.user.testcase;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import org.junit.jupiter.api.Test;
 import service.user.api.User;
 
+import java.io.*;
 import java.util.HashMap;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 
 public class TestUser {
 
@@ -51,20 +56,54 @@ public class TestUser {
     }
 
     @Test
-    public void createFromTemplate(){
+    public void createFromTemplate() throws IOException {
 
-        String userId = "zht"+System.currentTimeMillis();
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        data.put("name", "Mustache");
+        data.put("mobile",String.valueOf(System.currentTimeMillis()).substring(0,11));
+        data.put("userid",System.currentTimeMillis());
+
+        Writer writer = new StringWriter();
+        MustacheFactory mf = new DefaultMustacheFactory();
+        ///Users/mac/IdeaProjects/BCAutomation/BCAutomation/src/main/resources/service/user/api/user.json
+        Mustache mustache = mf.compile(this.getClass().getResource("/service/user/api/user.json").getPath());
+        mustache.execute(writer, data);
+        writer.flush();
+        System.out.println(writer.toString());
+    }
+
+    @Test
+    public void cloneUser(){
+
+        String userId = "zhm"+System.currentTimeMillis();
         String newName = "name for testing1";
         HashMap data = new HashMap();
         data.put("name",newName);
         data.put("address","address for testing");
-        data.put("department",new int[]{1});
+        //data.put("department",new int[]{1});
         data.put("mobile",String.valueOf(System.currentTimeMillis()).substring(0,11));
         User user= new User();
 
-        user.create(userId,data).then().body("errcode",equalTo(0));
+        user.clone(userId,data).then().body("errcode",equalTo(0));
         user.get(userId).then().body("name",equalTo(newName));
+    }
 
+    @Test
+    public void delete(){
+
+        String userId = "zhy"+System.currentTimeMillis();
+        String newName = "name for testing1";
+        HashMap data = new HashMap();
+        data.put("name",newName);
+        data.put("address","address for testing");
+        //data.put("department",new int[]{1});
+        data.put("mobile",String.valueOf(System.currentTimeMillis()).substring(0,11));
+        User user= new User();
+
+        user.clone(userId,data).then().body("errcode",equalTo(0));
+        //user.get(userId).then().body("name",equalTo(newName));
+        user.delete(userId).then().body("errmsg",equalTo("deleted"));
+        user.get(userId).then().body("errcode",not(equalTo(newName)));
     }
 
 }
